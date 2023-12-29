40092db06c54be0fd55b626fc38fd151d57144e5
kaylendog <actuallyori@gmail.com>
2023-12-29 16:50:55 +0000

    chore: fixup eslint config; gitignore

--- apps/frontend/components/registry.tsx
+++ apps/frontend/components/registry.tsx
@@ -5,8 +5,10 @@ import React, { useState } from "react";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
 export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
-	// Only create stylesheet once with lazy initial state
-	// x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+	/*
+	 * Only create stylesheet once with lazy initial state
+	 * x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+	 */
 	const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
 	useServerInsertedHTML(() => {
